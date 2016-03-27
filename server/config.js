004cf330cec56175af766f475986e7490e7fa97c
treesawittree <sawittreenakae@gmail.com>
2016-03-27 12:55:30 +0700

    update ip DB

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
+  mongoURL: process.env.MONGO_URL || 'mongodb://54.201.210.88:27017/mern-starter',
   port: process.env.PORT || 8000,
 };
 
